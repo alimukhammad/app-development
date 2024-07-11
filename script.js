aa33add5135735fdbc26a0545f1f4a23accacbe0
sardor <alimukhammad1@gmail.com>
2024-07-11 19:25:33 -0400

    1

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ let shape = document.querySelector('.shapeDiv');
 let btnSquare = document.querySelector('.btnSquare');
 let btnCircle = document.querySelector('.btnCircle');
 let btnRectangle = document.querySelector('.btnRectangle');
-
-
-btnCircle.addEventListener('click', function(){
-    console.log('Circle');
-});
-
-btnRectangle.addEventListener('click', function(){
-    console.log('Rectangle');
-});
+let displayShape = document.querySelector('.shapeDiv p');
 
 btnSquare.addEventListener('click', function(){
     shape.classList = "square"
+    displayShape.textContent = "square".toUpperCase();
     console.log('Square');
 });
+
+btnRectangle.addEventListener('click', function(){
+    shape.classList = "rectangle"
+    displayShape.textContent = "rectangle".toUpperCase();
+    console.log('Rectangle');
+});
+
+btnCircle.addEventListener('click', function(){
+    shape.classList = "circle"
+    displayShape.textContent = "circle".toUpperCase();
+    console.log('Circle');
+});
+
