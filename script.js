b78bce5998d308a43fdee994835a3eaaf6cac4b1
sardor <alimukhammad1@gmail.com>
2024-07-11 23:40:23 -0400

    1

--- script.js
+++ script.js
@@ -5,20 +5,33 @@ let btnRectangle = document.querySelector('.btnRectangle');
 let displayShape = document.querySelector('.shapeDiv p');
 
 btnSquare.addEventListener('click', function(){
+    console.log('Square');
     shape.classList = "square"
     displayShape.textContent = "square".toUpperCase();
-    console.log('Square');
 });
 
 btnRectangle.addEventListener('click', function(){
+    console.log('Rectangle');
     shape.classList = "rectangle"
     displayShape.textContent = "rectangle".toUpperCase();
-    console.log('Rectangle');
 });
 
 btnCircle.addEventListener('click', function(){
+    console.log('Circle');
     shape.classList = "circle"
     displayShape.textContent = "circle".toUpperCase();
-    console.log('Circle');
 });
 
+// dropdown Menu
+
+let menuBtn = document.querySelector('.menuBtn');
+let collapseMenu = document.querySelector('.collapseMenu');
+
+menuBtn.addEventListener('click', function(){
+    if(collapseMenu.style.display === "block"){
+        collapseMenu.style.display = "none";
+        return;
+    }
+
+    collapseMenu.style.display = "block";
+});
